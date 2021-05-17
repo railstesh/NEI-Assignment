ffc75377f69d81c3fe745b3c27755eb9d84c96e1
railstesh <railsriteshc56@gmail.com>
2021-05-17 19:31:22 +0530

    created dummy data

---
+++ src/common/listData.js
@@ -0,0 +1,32 @@
+export const listData = [
+  {
+    name: 'William Doe',
+    event: 'Warmup',
+    time: {
+      start: '4:30 AM',
+      end: '5:00 AM',
+    },
+    phoneNumber: '+48 603 433 955',
+    email: 'contact@codegate.pl',
+  },
+  {
+    name: 'Jon Doe',
+    event: '1:1',
+    time: {
+      start: '5:30 AM',
+      end: '6:00 AM',
+    },
+    phoneNumber: '+48 603 433 956',
+    email: 'support@codegate.pl',
+  },
+  {
+    name: 'Thomas Doe',
+    event: 'VIP',
+    time: {
+      start: '6:30 AM',
+      end: '7:00 AM',
+    },
+    phoneNumber: '+48 603 433 957',
+    email: 'info@codegate.pl',
+  },
+];
